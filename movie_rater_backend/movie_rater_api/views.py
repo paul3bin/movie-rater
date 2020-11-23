8813b6c18d1c5d319d6593dedebfea653002f398
Ebin Paul <ebinpaul@outlook.com>
2020-11-23 18:17:24 +0530

    Create views.py

---
+++ movie_rater_backend/movie_rater_api/views.py
@@ -0,0 +1,13 @@
+from .serializers import MovieSerializer
+from django.shortcuts import render
+from . import models, serializers
+from rest_framework import viewsets
+
+
+class MovieViewSet(viewsets.ModelViewSet):
+    queryset = models.Movie.objects.all()
+    serializer_class = (serializers.MovieSerializer,)
+
+class RatingViewSet(viewsets.ModelViewSet):
+    queryset = models.Rating.objects.all()
+    serializer_class = (serializers.RatingSerializer,)
