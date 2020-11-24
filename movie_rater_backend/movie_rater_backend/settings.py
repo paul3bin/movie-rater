c98afd45c2ebe4ea90789fbb22e6d726d3727551
Ebin Paul <ebinpaul@outlook.com>
2020-11-24 11:21:38 +0530

    Update settings.py

--- movie_rater_backend/movie_rater_backend/settings.py
+++ movie_rater_backend/movie_rater_backend/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
     'movie_rater_api',
 ]
 
@@ -120,3 +121,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# Settings for the REST framework
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': {
+        'rest_framework.permissions.IsAuthenticated',
+    } 
+}
