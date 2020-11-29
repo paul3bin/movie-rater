f4a501d5bca40e22f1349fb9a66ffd04b04258b6
Ebin Paul <ebinpaul@outlook.com>
2020-11-29 08:58:45 +0530

    Update views.py

--- movie_rater_backend/movie_rater_api/views.py
+++ movie_rater_backend/movie_rater_api/views.py
@@ -1,13 +1,60 @@
-from .serializers import MovieSerializer
+from . import serializers
 from django.shortcuts import render
 from . import models, serializers
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from rest_framework.decorators import action
+from rest_framework.authentication import TokenAuthentication
+from django.contrib.auth.models import User
+from rest_framework.permissions import IsAuthenticated, AllowAny
 
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = serializers.UserSerializer
 
 class MovieViewSet(viewsets.ModelViewSet):
     queryset = models.Movie.objects.all()
     serializer_class = serializers.MovieSerializer
+    authentication_classes = (TokenAuthentication, )
+    permission_classes = (AllowAny, )
+
+    @action(detail=True, methods=['POST'])
+    def rate_movie(self, request, pk=None):
+        if 'stars' in request.data:
+            movie = models.Movie.objects.get(id=pk)
+            stars = request.data['stars']
+            user = request.user
+            # user = User.objects.get(id=1)
+
+            try:
+                rating = models.Rating.objects.get(user=user.id, movie=movie.id)
+                rating.stars = stars
+                rating.save()
+                serializer = serializers.RatingSerializer(rating, many=False)
+                response = {'message': 'Rating Updated', 'result': serializer.data}
+                return Response(response, status=status.HTTP_200_OK)
+            
+            except:
+                rating = models.Rating.objects.create(user=user, movie=movie, stars=stars)
+                serializer = serializers.RatingSerializer(rating, many=False)
+                response = {'message': 'Rating Created', 'result': serializer.data}
+                return Response(response, status=status.HTTP_200_OK)
+            
+            else:
+                response = {'message': 'Something Went Wrong'}
+                return Response(response, status=status.HTTP_400_BAD_REQUEST)
+
 
 class RatingViewSet(viewsets.ModelViewSet):
     queryset = models.Rating.objects.all()
     serializer_class = serializers.RatingSerializer
+    authentication_classes = (TokenAuthentication, )
+    permission_classes = (IsAuthenticated, )
+
+    def update(self, request, *args, **kwargs):
+        response = {'message': 'Cannot Update!'}
+        return Response(response, status=status.HTTP_400_BAD_REQUEST)
+
+    def create(self, request, *args, **kwargs):
+        response = {'message': 'Cannot Create!'}
+        return Response(response, status=status.HTTP_400_BAD_REQUEST)
