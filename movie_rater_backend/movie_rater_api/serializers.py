048cf0a44edff79694aeab1bc97517bce3641152
Ebin Paul <ebinpaul@outlook.com>
2020-11-29 08:58:36 +0530

    Update serializers.py

--- movie_rater_backend/movie_rater_api/serializers.py
+++ movie_rater_backend/movie_rater_api/serializers.py
@@ -1,10 +1,25 @@
 from rest_framework import serializers
 from . import models
+from django.contrib.auth.models import User
+from rest_framework.authtoken.models import Token
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'password')
+        extra_kwargs = {'password': {'write_only': True, 'required': True}}
+    
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        Token.objects.create(user=user)
+        return user
+
 
 class MovieSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Movie
-        fields = ('id', 'title', 'description')
+        fields = ('id', 'title', 'genre', 'description', 'number_of_ratings', 'average_rating')
+
 
 class RatingSerializer(serializers.ModelSerializer):
     class Meta:
