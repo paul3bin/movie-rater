d27842e81304da165d557c75e3b3995aa1380897
Ebin Paul <ebinpaul@outlook.com>
2020-12-03 10:51:57 +0530

    Update movie-list.js

--- movie_rater_frontend/src/components/movie-list.js
+++ movie_rater_frontend/src/components/movie-list.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEdit, faTrash } from '@fortawesome/free-solid-svg-icons'
 
 function MovieList(props){
 
     const onMovieClick = movie => evt => {
         props.movieClicked(movie)
+    }
+
+    const editClicked = movie => {
+        props.editMovie(movie);
     }
 
     return (
@@ -13,6 +19,8 @@ function MovieList(props){
               return (
                   <div key={movie.id}>
                       <h2 onClick={onMovieClick(movie)}>{movie.title}</h2>
+                      <FontAwesomeIcon icon={faEdit} onClick={()=>editClicked()} className = {"Icons"}/>
+                      <FontAwesomeIcon icon={faTrash} className = {"Icons"}/>
                   </div>
               )
             })}
