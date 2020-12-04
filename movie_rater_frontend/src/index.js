8405cb68cfe15c68e45579b6b2976661d8cea03a
Ebin Paul <ebinpaul@outlook.com>
2020-12-04 11:11:45 +0530

    Update index.js

--- movie_rater_frontend/src/index.js
+++ movie_rater_frontend/src/index.js
@@ -3,13 +3,26 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {Route, BrowserRouter} from 'react-router-dom'
+import {Auth} from './components/auth'
+import {CookiesProvider} from 'react-cookie';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+
+function Router(){
+
+  return (
+    <React.StrictMode>
+      <CookiesProvider>
+        <BrowserRouter>
+          <Route exact path="/" component={Auth} />
+          <Route exact path="/movies" component={App} />
+        </BrowserRouter>
+      </CookiesProvider>
+    </React.StrictMode>
+  )
+}
+
+ReactDOM.render(<Router />, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
