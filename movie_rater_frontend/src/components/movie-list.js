9a0d7f78369a276ec395114294b90c79cab8557d
Ebin Paul <ebinpaul@outlook.com>
2020-12-04 11:11:39 +0530

    Update movie-list.js

--- movie_rater_frontend/src/components/movie-list.js
+++ movie_rater_frontend/src/components/movie-list.js
@@ -1,8 +1,12 @@
 import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faEdit, faTrash } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEdit, faTrash } from '@fortawesome/free-solid-svg-icons';
+import {API} from '../api-service';
+import {useCookies} from 'react-cookie';
 
 function MovieList(props){
+
+    const [token] = useCookies(['token']);
 
     const onMovieClick = movie => evt => {
         props.movieClicked(movie)
@@ -12,15 +16,21 @@ function MovieList(props){
         props.editMovie(movie);
     }
 
+    const removeClicked = movie => {
+        API.deleteMovie(movie.id, token['token'])
+        .then( () => props.removeMovie(movie) )
+        .catch(error => console.log(error))
+    }
+
     return (
         <div>
             Movie List
             {props.movies && props.movies.map(movie => {
               return (
-                  <div key={movie.id}>
-                      <h2 onClick={onMovieClick(movie)}>{movie.title}</h2>
-                      <FontAwesomeIcon icon={faEdit} onClick={()=>editClicked()} className = {"Icons"}/>
-                      <FontAwesomeIcon icon={faTrash} className = {"Icons"}/>
+                  <div key={movie.id} className = {'Movie-Item'}>
+                      <h2 className='Movie-title' onClick={onMovieClick(movie)}>{movie.title}</h2>
+                      <FontAwesomeIcon icon={faEdit} onClick={()=>editClicked(movie)} className="Icons"/>
+                      <FontAwesomeIcon icon={faTrash} onClick={()=>removeClicked(movie)} className="Icons"/>
                   </div>
               )
             })}
