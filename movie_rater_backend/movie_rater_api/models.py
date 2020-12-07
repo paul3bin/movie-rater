2178f74ae61eee7ba263d4d6afc5162e8e8166eb
Ebin Paul <ebinpaul@outlook.com>
2020-12-07 10:19:44 +0530

    Update models.py

--- movie_rater_backend/movie_rater_api/models.py
+++ movie_rater_backend/movie_rater_api/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.core.validators import MaxValueValidator, MaxValueValidator, MinValueValidator
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 class Movie(models.Model):
     title = models.CharField(max_length=200)
